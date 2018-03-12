19a1fb4d81a981eb9c14ca9049bd70957aba6530
wchang28 <wchang28@hotmail.com>
2018-03-12 12:07:04 -0400

    added component_mget to hc test

--- js/test/testHC.js
+++ js/test/testHC.js
@@ -42,8 +42,45 @@ var client = api.Client.init(function () {
         apiSecret: hc_api_secret
     }));
 });
+/*
 client.api("/v2/property/geocode")
-    .query({ "address": "43 Valmonte Plaza", "zipcode": "90274" })
+.query({"address": "43 Valmonte Plaza", "zipcode": "90274"})
+.get()
+.then((value: any) => {
+    console.log(`ret=\n${JSON.stringify(value, null, 2)}`);
+}).catch((err: api.IError) => {
+    console.error(`!!! Error: ${JSON.stringify(err)}`);
+});
+*/
+var allPropertyComponents = [
+    "property/census",
+    "property/details",
+    "property/details_enhanced",
+    "property/flood",
+    "property/listing_status",
+    "property/ltv_details",
+    "property/ltv_origination",
+    "property/mortgage_lien",
+    "property/mortgage_lien_all",
+    "property/nod",
+    "property/on_market",
+    "property/owner_occupied",
+    "property/rental_listing_status",
+    "property/rental_on_market",
+    "property/rental_value",
+    "property/rental_value_within_block",
+    "property/rental_yield",
+    "property/sales_history",
+    "property/school",
+    "property/value",
+    "property/value_by_quality",
+    "property/value_details_adjusted",
+    "property/value_forecast",
+    "property/value_hpi_adjusted",
+    "property/value_within_block"
+];
+client.api("/v2/property/component_mget")
+    .query({ "address": "43 Valmonte Plaza", "zipcode": "90274", "components": allPropertyComponents.join(",") })
     .get()
     .then(function (value) {
     console.log("ret=\n" + JSON.stringify(value, null, 2));
